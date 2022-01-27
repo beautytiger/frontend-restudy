5652e11a66a9a68fa5f2b5ed45ea1e96f01ed073
beautytiger <konmyn@gmail.com>
2022-01-27 23:14:25 +0800

    vue3响应式数据

--- 03_vue/vue3_study/src/App.vue
+++ 03_vue/vue3_study/src/App.vue
@@ -1,42 +1,40 @@
 <template>
-  <h2>setup和ref的基本使用</h2>
-  <h3>{{count}}</h3>
-  <button @click="updateCount">更新数据</button>
+<h2>reactive的使用</h2>
+<h3>名字{{user.name}}</h3>
+<h3>年龄{{user.age}}</h3>
+<h3>媳妇{{user.wife}}</h3>
+<hr>
+<button @click="updateUser">更新数据</button>
 </template>
 
 <script lang="ts">
-import { defineComponent,ref } from 'vue';
+import { defineComponent,reactive,ref } from 'vue';
 export default defineComponent({
   name: 'App',
-  //页面打开后，可以直接看到一个数据，点击按钮后，数据发生变化
-
-  //vue2的实现
-  // data() {
-  //   return {
-  //     count: 0
-  //   }
-  // },
-  // methods: {
-  //   updateCount() {
-  //     this.count++
-  //   }
-  // }
-
-  //vue3的实现方式
-  //setup是组合api的入口
   setup() {
-    // let count = 0 // 此数据不是响应式数据，数据变化，页面无法跟着渲染
-    const count = ref(0)
-    console.log(count)
-    function updateCount() {
-      console.log("更新数据")
-      // count++
-      count.value++
+    const obj = {
+      name: "小明",
+      age: 20,
+      wife: {
+        name:"小甜甜",
+        age:18,
+        cars:["奔驰", "宝马", "奥迪"]
+      }
     }
-    //返回一个对象
+    const user = reactive (obj)
+    const updateUser = ()=>{
+      //直接使用目标对象的方式来更新目标对象中的成员值，是不行的
+      // obj.name += "--"
+      //只能使用代理对象的方式
+      user.name += "--"
+      user.age +=1
+      user.wife.name += "--"
+      user.wife.age += 2
+      user.wife.cars[0] += "-"
+    }
     return {
-      count,
-      updateCount
+      user,
+      updateUser
     }
   }
 });
