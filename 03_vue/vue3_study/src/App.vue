1392d1eb85a315855a644a220889fe68f63617ca
beautytiger <konmyn@gmail.com>
2022-01-27 22:25:40 +0800

    setup 和ref的基本使用

--- 03_vue/vue3_study/src/App.vue
+++ 03_vue/vue3_study/src/App.vue
@@ -1,24 +1,43 @@
 <template>
-  <div>我又回来了</div>
-  <h1>{{ number }}</h1>
+  <h2>setup和ref的基本使用</h2>
+  <h3>{{count}}</h3>
+  <button @click="updateCount">更新数据</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent,ref } from 'vue';
 export default defineComponent({
   name: 'App',
-  //组合api中第一个要使用的函数
+  //页面打开后，可以直接看到一个数据，点击按钮后，数据发生变化
+
+  //vue2的实现
+  // data() {
+  //   return {
+  //     count: 0
+  //   }
+  // },
+  // methods: {
+  //   updateCount() {
+  //     this.count++
+  //   }
+  // }
+
+  //vue3的实现方式
+  //setup是组合api的入口
   setup() {
-    console.log("我来了")
-    const number = 10
+    // let count = 0 // 此数据不是响应式数据，数据变化，页面无法跟着渲染
+    const count = ref(0)
+    console.log(count)
+    function updateCount() {
+      console.log("更新数据")
+      // count++
+      count.value++
+    }
+    //返回一个对象
     return {
-      number
+      count,
+      updateCount
     }
   }
 });
 </script>
-
-<style>
-
-</style>
