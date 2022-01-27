b9e9bc027a1e2cf619aa0e311c4677c269184dbd
beautytiger <konmyn@gmail.com>
2022-01-27 20:45:35 +0800

    补充ts遗漏部分

--- 02_webpack_ts/src/08_泛型.ts
+++ 02_webpack_ts/src/08_泛型.ts
@@ -55,4 +55,67 @@
     }
     const a1 = getMsg<string, number>("python", 99)
     console.log(a1[0].split(""), a1[1].toFixed(2))
+
+    //泛型接口
+    interface IBaseCRUD<T>{
+        data:Array<T>
+        add:(t:T)=>T
+        getUserId:(id,number)=>T
+    }
+    class User {
+        id?:number
+        name:string
+        age:number
+        constructor(name:string, age:number) {
+            this.name = name
+            this.age = age
+        }
+    }
+    class UserCRUD implements IBaseCRUD<User>{
+        data:Array<User>=[]
+        add(user:User):User{
+            user.id = Date.now()+Math.random()
+            this.data.push(user)
+            return user
+        }
+        getUserId(id:number):User{
+            return this.data.find(user=>user.id===id)
+        }
+    }
+    //实例化添加用户信息的类
+    const userCRUD:UserCRUD = new UserCRUD()
+    userCRUD.add(new User("tom", 10))
+    userCRUD.add(new User("jack", 13))
+    userCRUD.add(new User("lucy", 14))
+    const {id} = userCRUD.add(new User("lily", 16))
+    userCRUD.add(new User("bill", 18))
+    console.log(userCRUD.data)
+    const user = userCRUD.getUserId(id)
+    console.log(user)
+
+    //泛型类
+    class GenericNumber<T>{
+        defaultValue:T
+        add:(x:T, y:T) => T
+    }
+    const g1:GenericNumber<number> = new GenericNumber<number>()
+    g1.defaultValue = 100
+    g1.add = function (x, y) {return x+ y}
+    console.log(g1.add(10, 20))
+    const g2:GenericNumber<string> = new GenericNumber<string>()
+    g2.defaultValue = "abc"
+    g2.add = function (x, y) {
+        return x+ y
+    }
+    console.log(g2.add("abc", "123"))
+
+    //泛型约束
+    interface ILength{
+        length:number
+    }
+    function getLength<T extends ILength>(x: T):number {
+        return x.length
+    }
+    console.log(getLength<string>("what are you doing"))
+    // console.log(getLength<number>(999)) 
 })()
