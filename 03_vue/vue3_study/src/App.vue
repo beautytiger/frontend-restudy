c9e333dad2f982b579320f547cb77c7228aea095
beautytiger <konmyn@gmail.com>
2022-02-09 20:49:15 +0800

    setup的执行时机问题

--- 03_vue/vue3_study/src/App.vue
+++ 03_vue/vue3_study/src/App.vue
@@ -1,41 +1,20 @@
 <template>
-<h2>reactive的使用</h2>
-<h3>名字{{user.name}}</h3>
-<h3>年龄{{user.age}}</h3>
-<h3>媳妇{{user.wife}}</h3>
+<h2>App父级组件</h2>
+<h3>msg:{{msg}}</h3>
+<button @click="msg += '==='">更新数据</button>
 <hr>
-<button @click="updateUser">更新数据</button>
+<child :msg="msg"/>
 </template>
-
 <script lang="ts">
-import { defineComponent,reactive,ref } from 'vue';
+import { defineComponent, ref } from 'vue'
+//引入子级组件
+import Child from './components/Child.vue'
 export default defineComponent({
-  name: 'App',
+  components: { Child },
+  name:"App",
   setup() {
-    const obj = {
-      name: "小明",
-      age: 20,
-      wife: {
-        name:"小甜甜",
-        age:18,
-        cars:["奔驰", "宝马", "奥迪"]
-      }
-    }
-    const user = reactive (obj)
-    const updateUser = ()=>{
-      //直接使用目标对象的方式来更新目标对象中的成员值，是不行的
-      // obj.name += "--"
-      //只能使用代理对象的方式
-      user.name += "--"
-      user.age +=1
-      user.wife.name += "--"
-      user.wife.age += 2
-      user.wife.cars[0] += "-"
-    }
-    return {
-      user,
-      updateUser
-    }
+    const msg = ref("what are you doing")
+    return {msg}
   }
-});
+})
 </script>
---
+++ 03_vue/vue3_study/src/components/Child.vue
@@ -0,0 +1,28 @@
+<template>
+<h2>child子级组件</h2>
+<h3>msg:{{msg}}</h3>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name:"Child",
+  props: ['msg'],
+  //setup细节问题
+  //setup是在beforecreate生命周期毁掉之前就执行了，而且额就执行一次
+  //由此可以推断出，setup在执行的时候，当前的组件还没有创建出来，也就意味着，组件实例对象this根本就不能用
+  //this 是undefined，就不能通过this调用data/computed/methods/props
+  //数据初始化的声明周期回调
+  beforeCreate(){
+      console.log("before create执行了");
+      
+  },
+  //界面渲染完毕
+//   mounted() {},
+  setup() {
+      console.log("setup执行了",this)
+      return {
+          //setup 中一般都是返回一个对象，对象中的属性和方法都可以在html模板中直接使用
+      }
+  }
+})
+</script>
