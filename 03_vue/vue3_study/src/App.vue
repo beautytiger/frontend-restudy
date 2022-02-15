5f5a0a9c58d7c5ce1c5361e2b0abdadda630812a
beautytiger <konmyn@gmail.com>
2022-02-15 19:02:37 +0800

    torefs usage

--- 03_vue/vue3_study/src/App.vue
+++ 03_vue/vue3_study/src/App.vue
@@ -1,55 +1,30 @@
 <template>
-  <h2>自定义hook函数</h2>
-  <h3>x;{{x}}, y:{{y}}</h3>
-  <h4 v-if="loading">正在加载中...</h4>
-  <h4 v-else-if="errorMsg">错误信息:{{ errorMsg }}</h4>
-  <ul v-else>
-    <li>id:{{ data.id }}</li>
-    <li>address:{{ data.address }}</li>
-    <li>distance:{{ data.distance }}</li>
-  </ul>
-  <hr>
-  <ul v-for="item in data" :key="item.id">
-    <li>id:{{ item.id }}</li>
-    <li>title:{{ item.title }}</li>
-    <li>price:{{ item.price }}</li>
-  </ul>
+  <h2>toRefs的使用</h2>
+  <!-- <h3>name: {{ state.name }}</h3>
+  <h3>name: {{ state.age }}</h3> -->
+  <h3>name: {{ name }}</h3>
+  <h3>name: {{ age }}</h3>
 </template>
 <script lang="ts">
-import { defineComponent, watch } from "vue";
-import useMousePostion from "./hooks/useMousePosition"
-import useRequests from "./hooks/useRequests"
-
-//定义接口约束对象类型
-// interface AddressData{
-//   id:number;
-//   address:string;
-//   distance:number;
-// }
-interface ProductData{
-  id:number;
-  title:string;
-  price:number;
-}
+import { defineComponent, reactive, toRefs } from "vue";
 export default defineComponent({
   name: "App",
-  //收集点击坐标
-  setup(){
-    const{ x, y } = useMousePostion()
-    // const{loading, data, errorMsg} = useRequests<AddressData>("/data/address.json")
-    const{loading, data, errorMsg} = useRequests<ProductData[]>("/data/products.json")
-    watch(data, ()=>{
-      if (data.value) {
-        console.log(data.value.length);
-      }
+  setup() {
+    const state = reactive({
+      name: "自来也",
+      age: 47
     })
+    const state2 = toRefs(state)
+    console.log(state2);
+    //定时器，更新数据，如果数据变化了，页面也会随之变化，就是响应式数据
+    setInterval(()=>{
+      state2.name.value += "-"
+      console.log("...");
+    }, 1000)
     return {
-      x,
-      y,
-      loading,
-      data,
-      errorMsg
+      ...state2,
     }
   }
 })
+
 </script>
