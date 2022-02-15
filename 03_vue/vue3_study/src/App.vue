8e2125a6bfb270527bfcfc9de844f80ebc88f73a
beautytiger <konmyn@gmail.com>
2022-02-15 19:08:36 +0800

    ref获取元素

--- 03_vue/vue3_study/src/App.vue
+++ 03_vue/vue3_study/src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <h2>toRefs的使用</h2>
-  <!-- <h3>name: {{ state.name }}</h3>
-  <h3>name: {{ state.age }}</h3> -->
-  <h3>name: {{ name }}</h3>
-  <h3>name: {{ age }}</h3>
+  <h2>ref获取页面中的元素</h2>
+  <input type="text" ref="inputRef">
 </template>
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 export default defineComponent({
   name: "App",
   setup() {
-    const state = reactive({
-      name: "自来也",
-      age: 47
+    //默认是空的，页面加载完毕，说明组件已经存在，获取文本框元素
+    const inputRef = ref<HTMLElement | null>(null)
+    onMounted(()=>{
+      inputRef.value && inputRef.value.focus() //自动获取焦点
     })
-    const state2 = toRefs(state)
-    console.log(state2);
-    //定时器，更新数据，如果数据变化了，页面也会随之变化，就是响应式数据
-    setInterval(()=>{
-      state2.name.value += "-"
-      console.log("...");
-    }, 1000)
     return {
-      ...state2,
+      inputRef,
     }
   }
 })
