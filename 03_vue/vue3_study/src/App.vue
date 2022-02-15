a7e7dc9b420957be6020fe6445dd0c40ac310d0a
beautytiger <konmyn@gmail.com>
2022-02-15 20:07:56 +0800

    readonly和shallowreadonly

--- 03_vue/vue3_study/src/App.vue
+++ 03_vue/vue3_study/src/App.vue
@@ -1,68 +1,33 @@
 <template>
-  <h2>shallowReactive和shallowRef</h2>
-  <h3>m1: {{ m1 }}</h3>
-  <h3>m2: {{ m2 }}</h3>
-  <h3>m3: {{ m3 }}</h3>
-  <h3>m4: {{ m4 }}</h3>
+  <h2>readonly和shallowreadonly</h2>
+  <h3>state: {{ state2 }}</h3>
   <hr>
-  <button @click="update">更新数据</button>
+  <button @click="update">更新</button>
 </template>
 <script lang="ts">
-import { defineComponent, reactive, ref, shallowReactive, shallowRef } from "vue";
+import { defineComponent, reactive, readonly, shallowReadonly } from "vue";
 export default defineComponent({
   name: "App",
   setup() {
-    //深度响应式数据
-    const m1 = reactive({
-      name: "鸣人",
+    const state = reactive({
+      name: "佐助",
       age: 20,
       car: {
-        name: "奔驰",
-        color: "ref",
+        name: "benz",
+        color: "yellow"
       }
     })
-    //浅监视
-    const m2 = shallowReactive({
-      name: "鸣人",
-      age: 20,
-      car: {
-        name: "奔驰",
-        color: "ref",
-      }
-    })
-    const m3 = ref({
-      name: "鸣人",
-      age: 20,
-      car: {
-        name: "奔驰",
-        color: "ref",
-      }
-    })
-    const m4 = shallowRef({
-      name: "鸣人",
-      age: 20,
-      car: {
-        name: "奔驰",
-        color: "ref",
-      }
-    })
-    console.log(m1, m2,m3,m4);
-    
+    const state2 = readonly(state)
+    const state3 = shallowReadonly(state)
     const update = ()=>{
-      m1.name += "..."
-      m1.car.name += "]]]"
-      m2.name += "..."
-      m2.car.name += "]]]" //奇怪，单独操作不起作用，一起操作就能起作用
-      m3.value.name += "..."
-      m3.value.car.name += "]]]"
-      m4.value.name += "..."
-      m4.value.car.name += "]]]"
+      console.log("测试")
+      // state2.name += "+++"
+      // state2.car.name += "+++"
+      // state3.name += "+++"
+      state3.car.name += "+++"
     }
     return {
-      m1,
-      m2,
-      m3,
-      m4,
+      state2,
       update
     }
   }
