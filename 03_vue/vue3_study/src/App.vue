26ddb6d61227fbefb3946a649b4285b66488c400
beautytiger <konmyn@gmail.com>
2022-02-15 19:22:35 +0800

    shallow reactive 和 ref

--- 03_vue/vue3_study/src/App.vue
+++ 03_vue/vue3_study/src/App.vue
@@ -1,19 +1,69 @@
 <template>
-  <h2>ref获取页面中的元素</h2>
-  <input type="text" ref="inputRef">
+  <h2>shallowReactive和shallowRef</h2>
+  <h3>m1: {{ m1 }}</h3>
+  <h3>m2: {{ m2 }}</h3>
+  <h3>m3: {{ m3 }}</h3>
+  <h3>m4: {{ m4 }}</h3>
+  <hr>
+  <button @click="update">更新数据</button>
 </template>
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
+import { defineComponent, reactive, ref, shallowReactive, shallowRef } from "vue";
 export default defineComponent({
   name: "App",
   setup() {
-    //默认是空的，页面加载完毕，说明组件已经存在，获取文本框元素
-    const inputRef = ref<HTMLElement | null>(null)
-    onMounted(()=>{
-      inputRef.value && inputRef.value.focus() //自动获取焦点
+    //深度响应式数据
+    const m1 = reactive({
+      name: "鸣人",
+      age: 20,
+      car: {
+        name: "奔驰",
+        color: "ref",
+      }
     })
+    //浅监视
+    const m2 = shallowReactive({
+      name: "鸣人",
+      age: 20,
+      car: {
+        name: "奔驰",
+        color: "ref",
+      }
+    })
+    const m3 = ref({
+      name: "鸣人",
+      age: 20,
+      car: {
+        name: "奔驰",
+        color: "ref",
+      }
+    })
+    const m4 = shallowRef({
+      name: "鸣人",
+      age: 20,
+      car: {
+        name: "奔驰",
+        color: "ref",
+      }
+    })
+    console.log(m1, m2,m3,m4);
+    
+    const update = ()=>{
+      m1.name += "..."
+      m1.car.name += "]]]"
+      m2.name += "..."
+      m2.car.name += "]]]" //奇怪，单独操作不起作用，一起操作就能起作用
+      m3.value.name += "..."
+      m3.value.car.name += "]]]"
+      m4.value.name += "..."
+      m4.value.car.name += "]]]"
+    }
     return {
-      inputRef,
+      m1,
+      m2,
+      m3,
+      m4,
+      update
     }
   }
 })
